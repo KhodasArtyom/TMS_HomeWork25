755b1b186fc46e37a24fbfb3e3c57533d6697c3c
ArtyomKhodas <a.Khodas1601@gmail.com>
2023-05-01 12:14:07 +0300

    add service and interaction with User

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,79 @@
+import by.tms.khodasartyom.homewokr25.comparator.ByNameComparator;
+import by.tms.khodasartyom.homewokr25.comparator.ByYearIssueComparator;
 import by.tms.khodasartyom.homewokr25.model.Show;
+import by.tms.khodasartyom.homewokr25.predicator.ByCountryOfIssuePredicate;
+import by.tms.khodasartyom.homewokr25.predicator.ByYearOfIssuePredicate;
 import by.tms.khodasartyom.homewokr25.repository.ShowRepository;
+import by.tms.khodasartyom.homewokr25.service.ShowService;
+import by.tms.khodasartyom.homewokr25.service.ShowServiceImpl;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class Main {
+    ShowRepository showRepository = new ShowRepository();
+
     public static void main(String[] args) {
         ShowRepository showRepository = new ShowRepository();
+        ShowServiceImpl service = new ShowServiceImpl(showRepository);
+
         List<Show> showList = showRepository.getAllShows();
 
-        for (Show show : showList) {
-            System.out.println(show.toString());
+        List<Comparator<Show>> sortingList = getSorting();
+        List<Predicate<Show>> filteringList = getFiltering();
+        List<Show> shows = service.request(sortingList, filteringList);
+        if (shows.isEmpty()) {
+            System.out.println("It is empty");
+        } else {
+            for (Show show : showList) {
+                System.out.println(show.toString());
+            }
         }
 
+
+    }
+
+    private static List<Comparator<Show>> getSorting() {
+        List<Comparator<Show>> sortingList = new ArrayList<>();
+        System.out.println("Sorting by:\n" +
+                "byName\n" +
+                "byYearIssue\n");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String request = scanner.nextLine();
+            if (request.equals("end")) {
+                return sortingList;
+            }
+            Comparator<Show> comparator = switch (request) {
+                case "byName" -> new ByNameComparator();
+                case "byYearIssue" -> new ByYearIssueComparator();
+                default -> throw new RuntimeException();
+            };
+            sortingList.add(comparator);
+        }
+    }
+
+    private static List<Predicate<Show>> getFiltering() {
+        List<Predicate<Show>> filters = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Filtering by:\n" +
+                "CountryOfIssue\n" +
+                "ByYearOFIssue\n" +
+                "end");
+        while (true) {
+            String request = scanner.nextLine();
+            String[] parts = request.split("\\s+");
+
+            if (parts[0].equals("end")) return filters;
+            Predicate<Show> filter = switch (parts[0]) {
+                case "byCountry" -> new ByCountryOfIssuePredicate(parts[1]);
+                case "byYearOfIssue" -> new ByYearOfIssuePredicate(Integer.parseInt(parts[2]));
+                default -> throw new IllegalStateException("Unexpected value: " + parts[0]);
+            };
+            filters.add(filter);
+        }
     }
 }
---
+++ src/main/java/by/tms/khodasartyom/homewokr25/service/ShowServiceImpl.java
@@ -0,0 +1,50 @@
+package by.tms.khodasartyom.homewokr25.service;
+
+import by.tms.khodasartyom.homewokr25.model.Show;
+import by.tms.khodasartyom.homewokr25.repository.AllShowsRepository;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ShowServiceImpl implements ShowService {
+private final AllShowsRepository repo;
+
+    public ShowServiceImpl(AllShowsRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public List<Show> request(List<Comparator<Show>> sorting, List<Predicate<Show>> filters) {
+        List<Show> allShows = repo.getAllShows();
+        useSorting(allShows,sorting);
+        useFilters(allShows,filters);
+        return allShows;
+    }
+
+
+    public void useSorting(List<Show> list, List<Comparator<Show>> sorting) {
+        if (!sorting.isEmpty()) {
+            Iterator<Comparator<Show>> iterator = sorting.iterator();
+            Comparator<Show> resultSorting = iterator.next();
+            while (iterator.hasNext()) {
+                Comparator<Show> next = iterator.next();
+                resultSorting = resultSorting.thenComparing(next);
+            }
+            list.sort(resultSorting);
+        }
+    }
+
+    public void useFilters(List<Show> shows, List<Predicate<Show>> filters) {
+        if (!filters.isEmpty()) {
+            Iterator<Predicate<Show>> iterator = filters.iterator();
+            Predicate<Show> resultFiltering = iterator.next();
+            while (iterator.hasNext()) {
+                Predicate<Show> next = iterator.next();
+                resultFiltering = resultFiltering.and(next);
+            }
+            shows.removeIf(resultFiltering.negate());
+        }
+    }
+}
